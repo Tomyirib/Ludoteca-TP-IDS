404e7e0fc37ac31be2c989dc4e067d5ed22646ff
Nahuel Villa <nahuel.matiasvilla@gmail.com>
2025-05-29 23:57:51 -0300

    feat(423): separe steam_service and aggregate database tables

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,12 @@
 from flask import Flask
 import requests
+from steam_service import fetch_game_data
 
 app = Flask(__name__)
 
 @app.route('/games/<int:game_id>', methods=['GET'])
 def get_game(game_id):
-    response = requests.get(f"https://store.steampowered.com/api/appdetails?appids={game_id}")
-    
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print(f"Error {response.status_code}: {response.text}")
-
+    return fetch_game_data(game_id)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
---
+++ backend/init_db.sql
@@ -0,0 +1,64 @@
+CREATE DATABASE IF NOT EXISTS ludoteca;
+USE ludoteca;
+
+CREATE TABLE IF NOT EXISTS juegos (
+    id_juego INT PRIMARY KEY,
+    nombre VARCHAR(255),
+    tipo VARCHAR(50),
+    es_gratis BOOLEAN,
+    edad_requerida INT,
+    sitio_web TEXT,
+    descripcion_detallada TEXT,
+    acerca_del_juego TEXT,
+    descripcion_corta TEXT,
+    imagen_cabecera TEXT,
+    imagen_capsula_231x87 TEXT,
+    imagen_capsula_184x69 TEXT
+);
+
+
+CREATE TABLE IF NOT EXISTS generos (
+    id_genero INT PRIMARY KEY,
+    descripcion VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS juego_genero (
+    id_juego INT REFERENCES juegos(id_juego),
+    id_genero INT REFERENCES generos(id_genero),
+    PRIMARY KEY (id_juego, id_genero)
+);
+
+-- Tabla de categorías
+CREATE TABLE IF NOT EXISTS categorias (
+    id INTEGER UNIQUE PRIMARY KEY,
+    descripcion TEXT UNIQUE
+);
+
+
+CREATE TABLE IF NOT EXISTS juego_categoria (
+    juego_id INTEGER,
+    categoria_id INTEGER,
+    PRIMARY KEY (juego_id, categoria_id),
+    FOREIGN KEY (juego_id) REFERENCES juegos(id) ON DELETE CASCADE,
+    FOREIGN KEY (categoria_id) REFERENCES categorias(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS requisitos_minimos (
+    id_requisito INT PRIMARY KEY,
+    id_juego INT REFERENCES juegos(id_juego),
+    sistema_operativo VARCHAR(100),
+    procesador VARCHAR(255),
+    memoria_ram VARCHAR(100),
+    almacenamiento VARCHAR(100),
+    tarjeta_grafica VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS requisitos_recomendados (
+    id_requisito INT PRIMARY KEY,
+    id_juego INT REFERENCES juegos(id_juego),
+    sistema_operativo VARCHAR(100),
+    procesador VARCHAR(255),
+    memoria_ram VARCHAR(100),
+    almacenamiento VARCHAR(100),
+    tarjeta_grafica VARCHAR(255)
+);
