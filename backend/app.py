45f68a1e9674cc76b13ed5ce79608d501f94d909
alesotelo <asotelo@fi.uba.ar>
2025-06-17 00:33:13 -0300

    ajustes post rebase #20 y #21

--- backend/app.py
+++ backend/app.py
@@ -2,8 +2,8 @@ from flask import Flask, request, jsonify
 import requests
 from steam_service import fetch_game_data, get_all_games_data
 from db_login import insert_user, login
-from routes.comentarios import comentarios_bp
 from iniciar_db import connect_db as get_db_connection
+from routes.comentarios import comentarios_bp
 
 app = Flask(__name__)
 
