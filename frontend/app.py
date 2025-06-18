39ba84c8d74578302876b26316ca9e37fbb75131
Nahuel Villa <nahuel.matiasvilla@gmail.com>
2025-06-18 18:08:23 -0300

    fix: replace port

--- frontend/app.py
+++ frontend/app.py
@@ -264,4 +264,4 @@ def post_comentario():
     return redirect(url_for('generic', game_id=redirect_id))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5002)
+    app.run(debug=True, port=5000)
