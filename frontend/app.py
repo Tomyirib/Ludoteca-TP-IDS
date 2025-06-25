a2c935e8982eca770eca595c38dc50fc364bdba7
Tomas <tomasirib30@gmail.com>
2025-06-25 18:09:27 -0300

    fix: error al loguearse en usuario inexistente

--- frontend/app.py
+++ frontend/app.py
@@ -64,7 +64,10 @@ def login():
             flash('Inicio de sesión exitoso', 'success')
             return redirect(url_for('index'))
         else:
-            mensaje = resp.json().get('error', 'Usuario o contraseña incorrectos')
+            try:
+                mensaje = resp.json().get('error', 'Usuario o contraseña incorrectos')
+            except Exception:
+                mensaje = 'Usuario o contraseña incorrectos'
             flash(mensaje, 'danger')
             return render_template('login.html', brand=BRAND, nombre=nombre)
     return render_template('login.html', brand=BRAND, nombre=nombre)
